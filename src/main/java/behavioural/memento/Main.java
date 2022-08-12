6cad818dda4c01ecb24a7cdcb5299320e01c6fc4
Mustafa <mustafa.ercan392@gmail.com>
2022-08-12 14:07:33 +0300

    Implement Memento Pattern

---
+++ src/main/java/behavioural/memento/Main.java
@@ -0,0 +1,21 @@
+package behavioural.memento;
+
+public class Main {
+    public static void main(String[] args){
+        Editor editor = new Editor();
+        History history = new History();
+
+        editor.setContent("a");
+        history.push(editor.createState());
+
+        editor.setContent("b");
+        history.push(editor.createState());
+
+        editor.setContent("c");
+        editor.restore(history.pop());
+        editor.restore(history.pop());
+
+        // The result should be "a" when we pop 2 times
+        System.out.println(editor.getContent());
+    }
+}
